3a6059582300cc80898b0ac3c6c2d04286421196
ctienru <ctienru@gmail.com>
2026-01-15 11:16:15 -0500

    initialize Spring Boot project with Maven wrapper

---
+++ src/main/java/com/example/podcast_backend/PodcastBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.podcast_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodcastBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PodcastBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/podcast_backend/PodcastBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.podcast_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodcastBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
